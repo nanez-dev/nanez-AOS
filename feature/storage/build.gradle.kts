a11619cbf68935c18d4170e27b3f6a0b11e7b614
ESeungJun <6951004@naver.com>
2023-11-02 10:27:23 +0900

    [feat] 컨벤션 및 보관함 모듈 추가

---
+++ feature/storage/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("convention.nane.module.base")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.nane.storage"
+}
+
+dependencies {
+    implementation(project(":base"))
+    implementation(project(":utils"))
+    implementation(project(":network"))
+
+    implementation(libs.bundles.module.home)
+    kapt(libs.bundles.hilt.compile.kapt)
+}
+
+kapt {
+    correctErrorTypes = true
+}
