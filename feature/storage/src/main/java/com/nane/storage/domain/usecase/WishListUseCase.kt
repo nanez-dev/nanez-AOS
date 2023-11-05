b5637a6aaeb104a1e6cb55db86a260fbd8a76add
Subin <rla0010101@gmail.com>
2023-11-05 19:33:31 +0900

    [feat] domain usecase 구현

--- feature/storage/src/main/java/com/nane/storage/domain/usecase/WishListUseCase.kt
+++ feature/storage/src/main/java/com/nane/storage/domain/usecase/WishListUseCase.kt
@@ -1,4 +1,25 @@
 package com.nane.storage.domain.usecase
 
-class WishListUseCase {
+import com.nane.base.data.DataResult
+import com.nane.base.data.DomainResult
+import com.nane.storage.data.repository.IWishListRepository
+import javax.inject.Inject
+
+class WishListUseCase @Inject constructor(
+    private val repository: IWishListRepository
+) {
+    suspend fun getMyList(type: String?): DomainResult<String> {
+        val dataResult = repository.getMyList(type)
+        return when (dataResult) {
+            is DataResult.Success -> {
+                DomainResult.Success(dataResult.data)
+            }
+            is DataResult.Failed -> {
+                DomainResult.Failed(dataResult.msg, dataResult.code)
+            }
+            is DataResult.Error -> {
+                DomainResult.Error(dataResult.exception)
+            }
+        }
+    }
 }
