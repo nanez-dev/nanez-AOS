b0cc991baec2d85d9eebc1aa83262d490539a642
Subin <rla0010101@gmail.com>
2023-11-13 22:49:53 +0900

    Merge branch 'develop' into feature/storage

--- feature/storage/src/main/java/com/nane/storage/data/source/impl/WishListRemoteSourceImpl.kt
+++ feature/storage/src/main/java/com/nane/storage/data/source/impl/WishListRemoteSourceImpl.kt
@@ -1,18 +1,15 @@
-package com.nane.storage.data.source.impl
+package com.nane.storage.data.source.Impl
 
+import com.nane.network.api.storage.StorageApi
 import com.nane.network.service.RetrofitPerfumeService
 import com.nane.storage.data.source.IWishListRemoteSource
 import retrofit2.Response
 import javax.inject.Inject
 
-/**
- * Created by haul on 11/4/23
- */
 class WishListRemoteSourceImpl @Inject constructor(
     private val perfumeService: RetrofitPerfumeService
-) : IWishListRemoteSource {
-
-    override suspend fun getMyList(type: String?): Response<String> {
+): IWishListRemoteSource {
+    override suspend fun getMyList(type: String?): Response<StorageApi.Response> {
         return perfumeService.getMyList(type)
     }
 }
