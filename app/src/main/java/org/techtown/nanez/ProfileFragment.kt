9a7ca5477782c7141aaa882f1e07bf56d8aff411
Subin <rla0010101@gmail.com>
2024-01-02 15:59:57 +0900

    [feat] 로그인 여부에 따른 마이페이지 화면 구성 차별화

--- app/src/main/java/org/techtown/nanez/ProfileFragment.kt
+++ app/src/main/java/org/techtown/nanez/ProfileFragment.kt
@@ -3,13 +3,13 @@ package org.techtown.nanez
 import androidx.fragment.app.viewModels
 import com.nane.base.view.BaseBindFragment
 import com.nane.base.viewmodel.BaseViewModel
-import org.techtown.nanez.databinding.ProfileFragmentBinding
+import org.techtown.nanez.databinding.NoLoginProfileFragmentBinding
 
-class ProfileFragment : BaseBindFragment<ProfileFragmentBinding, BaseViewModel>(R.layout.profile_fragment) {
+class ProfileFragment : BaseBindFragment<NoLoginProfileFragmentBinding, BaseViewModel>(R.layout.no_login_profile_fragment) {
 
     override fun createViewModel() = viewModels<BaseViewModel>().value
 
-    override fun initFragment(dataBinding: ProfileFragmentBinding, viewModel: BaseViewModel) {
+    override fun initFragment(dataBinding: NoLoginProfileFragmentBinding, viewModel: BaseViewModel) {
 
     }
 }
